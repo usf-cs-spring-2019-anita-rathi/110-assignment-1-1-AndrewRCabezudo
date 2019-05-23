bbc6e9f60248c23ccd0f5557231552a00915973d
Andrew Cabezudo <arcabezudo@dons.usfca.edu>
2019-05-23 15:47:16 -0700

    first

---
+++ assignment 1.1.py
@@ -0,0 +1,15 @@
+Seconds=float(input("Enter Seconds Here:"))
+Hours=(Seconds/3600)
+Minutes=(Seconds/60)
+Seconds2=(Seconds%60)
+print("Hours:",Hours)
+print("Minutes:",Minutes)
+print("Seconds:",Seconds2)
+
+print("This is problem 2")
+Future_Value=10000
+Ar=2.99
+Years=10
+Present_Value=float(Future_Value/((1+Ar)**Years))
+print(Present_Value)
+
